3dfa4c4cb5eba554255f91dfcfa7de2b5bece5cb
wael ghannem <wghannem@exoplatform.com>
2019-05-29 15:11:06 +0100

    fix/ECMS : clean code.

--- core/publication/src/test/java/org/exoplatform/services/wcm/publication/TestWCMComposer.java
+++ core/publication/src/test/java/org/exoplatform/services/wcm/publication/TestWCMComposer.java
@@ -22,191 +22,226 @@ import static org.exoplatform.services.wcm.publication.WCMComposer.*;
 
 public class TestWCMComposer extends BasePublicationTestCase {
 
-	WCMComposer               wcmComposer             = null;
-	PublicationService publicationService;
-	WCMComposerImpl wcmComposerService;
-	private PublicationPlugin plugin_;
+  WCMComposer               wcmComposer = null;
 
+  PublicationService        publicationService;
 
-	public void setUp() throws Exception {
-		super.setUp();
-		wcmComposer = container.getComponentInstanceOfType(WCMComposer.class);
-		publicationService = WCMCoreUtils.getService(PublicationService.class);
-		wcmComposerService = WCMCoreUtils.getService(WCMComposerImpl.class);
-		plugin_ = new DumpPublicationPlugin();
-		plugin_.setName("Simple");
-		plugin_.setDescription("Simple");
-		publicationService.addPublicationPlugin(plugin_);
-		applySystemSession();
-	}
+  WCMComposerImpl           wcmComposerService;
 
-	/**
-	 * test getContent for an authorized node
-	 * @throws Exception
-	 */
-	public void testGetContentAuthorized() throws Exception {
-		// Given
-		HashMap<String, String> filters = new HashMap<>();
+  private PublicationPlugin plugin_;
 
-		String nodeIdentifier = "/sites content";
+  public void setUp() throws Exception {
+    super.setUp();
+    wcmComposer = container.getComponentInstanceOfType(WCMComposer.class);
+    publicationService = WCMCoreUtils.getService(PublicationService.class);
+    wcmComposerService = WCMCoreUtils.getService(WCMComposerImpl.class);
+    plugin_ = new DumpPublicationPlugin();
+    plugin_.setName("Simple");
+    plugin_.setDescription("Simple");
+    publicationService.addPublicationPlugin(plugin_);
+    applySystemSession();
+  }
 
-		// When
-		Node node = wcmComposer.getContent(COLLABORATION_WS, nodeIdentifier, filters, sessionProvider);
+  /**
+   * test getContent for an authorized node
+   *
+   * @throws Exception
+   */
+  public void testGetContentAuthorized() throws Exception {
+    // Given
+    HashMap<String, String> filters = new HashMap<>();
 
-		// Then
-		assertNotNull(node);
-	}
+    String nodeIdentifier = "/sites content";
 
-	/**
-	 * test getContent for an non authorized node
-	 * @throws Exception
-	 */
-	public void testGetContentNotAuthorized() throws Exception {
-		// Given
-		HashMap<String, String> filters = new HashMap<>();
+    // When
+    Node node = wcmComposer.getContent(COLLABORATION_WS, nodeIdentifier, filters, sessionProvider);
 
-		String nodeIdentifier = "/exo:application";
+    // Then
+    assertNotNull(node);
+  }
 
-		// When
-		Node node = wcmComposer.getContent(COLLABORATION_WS, nodeIdentifier, filters, sessionProvider);
+  /**
+   * test getContent for an non authorized node
+   *
+   * @throws Exception
+   */
+  public void testGetContentNotAuthorized() throws Exception {
+    // Given
+    HashMap<String, String> filters = new HashMap<>();
 
-		// Then
-		assertNull(node);
-	}
+    String nodeIdentifier = "/exo:application";
 
-	/**
-	 * test getPaginatedContents result size for an authorized node
-	 * @throws Exception
-	 */
-	public void testShouldReturnPublicContentsWhenAdminUser() throws Exception {
-		// Given
-		HashMap<String, String> filters = new HashMap<>();
-		String folderPath = "repository:collaboration:/sites content/live/web contents/site artifacts";
-		NodeLocation  nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
+    // When
+    Node node = wcmComposer.getContent(COLLABORATION_WS, nodeIdentifier, filters, sessionProvider);
 
-		// When
-		Result result = wcmComposer.getPaginatedContents(nodeLocation, filters, sessionProvider);
+    // Then
+    assertNull(node);
+  }
 
-		// Then
-		assertEquals(1, result.getNumTotal());
-	}
+  /**
+   * test getPaginatedContents result size for an authorized node
+   *
+   * @throws Exception
+   */
+  public void testShouldReturnPublicContentsWhenAdminUser() throws Exception {
+    // Given
+    HashMap<String, String> filters = new HashMap<>();
+    String folderPath = "repository:collaboration:/sites content/live/web contents/site artifacts";
+    NodeLocation nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
 
-	public void testShouldReturnPublicContentsWhenPublicModeAndNonAdminUser() throws Exception {
-		// When
-		applyUserSession("marry", "gtn", "collaboration");
-		HashMap<String, String> filters = new HashMap<>();
-		filters.put(FILTER_MODE, MODE_LIVE);
-		filters.put(FILTER_VISIBILITY, VISIBILITY_PUBLIC);
-		String folderPath = "repository:collaboration:/sites content/live/web contents/site artifacts";
-		NodeLocation  nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
+    // When
+    Result result = wcmComposer.getPaginatedContents(nodeLocation, filters, sessionProvider);
 
-		// When
-		Result result = wcmComposer.getPaginatedContents(nodeLocation, filters, sessionProvider);
+    // Then
+    assertEquals(1, result.getNumTotal());
+  }
 
-		// Then
-		assertEquals(1, result.getNumTotal());
-	}
+  public void testShouldReturnPublicContentsWhenPublicModeAndNonAdminUser() throws Exception {
+    // When
+    applyUserSession("marry", "gtn", "collaboration");
+    HashMap<String, String> filters = new HashMap<>();
+    filters.put(FILTER_MODE, MODE_LIVE);
+    filters.put(FILTER_VISIBILITY, VISIBILITY_PUBLIC);
+    String folderPath = "repository:collaboration:/sites content/live/web contents/site artifacts";
+    NodeLocation nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
 
-	/**
-	 * test getPaginatedContents result size when FILTER_TOTAL is set
-	 * @throws Exception
-	 */
-	public void testPaginatedContentsWithFilter() throws Exception{
-		// Given
-		HashMap<String, String> filters = new HashMap<>();
-		String folderPath = "repository:collaboration:/sites content/live";
-		NodeLocation  nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
-		//test if FILTER_TOTAL value is already set
-		filters.put(WCMComposer.FILTER_TOTAL,"2");
+    // When
+    Result result = wcmComposer.getPaginatedContents(nodeLocation, filters, sessionProvider);
 
-		// When
-		Result result = wcmComposer.getPaginatedContents(nodeLocation, filters, sessionProvider);
+    // Then
+    assertEquals(1, result.getNumTotal());
+  }
 
-		// Then
-		assertEquals(2, result.getNumTotal());
-	}
+  /**
+   * test getPaginatedContents result size when FILTER_TOTAL is set
+   *
+   * @throws Exception
+   */
+  public void testPaginatedContentsWithFilter() throws Exception {
+    // Given
+    HashMap<String, String> filters = new HashMap<>();
+    String folderPath = "repository:collaboration:/sites content/live";
+    NodeLocation nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
+    // test if FILTER_TOTAL value is already set
+    filters.put(WCMComposer.FILTER_TOTAL, "2");
 
+    // When
+    Result result = wcmComposer.getPaginatedContents(nodeLocation, filters, sessionProvider);
 
-	public void testgetPaginatedContentsShouldRemoveDuplicatedNodes() throws Exception {
-		HashMap<String, String> context = new HashMap<String, String>();
-		context.put("visibility", "true");
+    // Then
+    assertEquals(2, result.getNumTotal());
+  }
 
-		HashMap<String, String> filters = new HashMap<>();
-		String folderPath = "repository:collaboration:/sites content/live";
-		NodeLocation  nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
-		String workspace = nodeLocation.getWorkspace();
-		Node rootNode = session.getRootNode();//getNode("/sites content/live");
+  public void testgetPaginatedContentsShouldRemoveDuplicatedNodes() throws Exception {
+    HashMap<String, String> context = new HashMap<String, String>();
+    context.put("visibility", "true");
 
-		Node nodeone_en = rootNode.addNode("nodeone_en", NodetypeConstant.EXO_WEBCONTENT);
-		nodeone_en.setProperty(NodetypeConstant.EXO_LANGUAGE, "en");
-		nodeone_en.setProperty(NodetypeConstant.EXO_TITLE,"node one en");
-		publicationService.enrollNodeInLifecycle(nodeone_en, plugin_.getLifecycleName());
-		publicationService.changeState(nodeone_en, PUBLISHED, context);
+    HashMap<String, String> filters = new HashMap<>();
+    String folderPath = "repository:collaboration:/sites content/live";
+    NodeLocation nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
+    String workspace = nodeLocation.getWorkspace();
+    Node rootNode = session.getRootNode();// getNode("/sites content/live");
 
-		Node nodeone_fr = rootNode.addNode("nodeone_fr", NodetypeConstant.EXO_WEBCONTENT);
-		nodeone_fr.setProperty(NodetypeConstant.EXO_LANGUAGE, "fr");
-		nodeone_fr.setProperty(NodetypeConstant.EXO_TITLE,"node one fr");
-		publicationService.enrollNodeInLifecycle(nodeone_fr, plugin_.getLifecycleName());
-		publicationService.changeState(nodeone_fr, PUBLISHED, context);
+    Node nodeone_en = rootNode.addNode("nodeone_en", NodetypeConstant.EXO_WEBCONTENT);
+    nodeone_en.setProperty(NodetypeConstant.EXO_LANGUAGE, "en");
+    nodeone_en.setProperty(NodetypeConstant.EXO_TITLE, "node one en");
+    publicationService.enrollNodeInLifecycle(nodeone_en, plugin_.getLifecycleName());
+    publicationService.changeState(nodeone_en, PUBLISHED, context);
 
-		NodeIterator iter = rootNode.getNodes();
-		WCMComposerImpl wcmComposerImplSpy = Mockito.spy(wcmComposerService);
-		int i =0;
-		while (i < 5){
-			iter.next();
-			i++;
-		}
-		List<Node> l1 = new ArrayList<Node>(){{add(nodeone_fr);}};
+    Node nodeone_fr = rootNode.addNode("nodeone_fr", NodetypeConstant.EXO_WEBCONTENT);
+    nodeone_fr.setProperty(NodetypeConstant.EXO_LANGUAGE, "fr");
+    nodeone_fr.setProperty(NodetypeConstant.EXO_TITLE, "node one fr");
+    publicationService.enrollNodeInLifecycle(nodeone_fr, plugin_.getLifecycleName());
+    publicationService.changeState(nodeone_fr, PUBLISHED, context);
 
-		List<Node> l2 = new ArrayList<Node>(){{add(nodeone_en);}};
+    NodeIterator iter = rootNode.getNodes();
+    WCMComposerImpl wcmComposerImplSpy = Mockito.spy(wcmComposerService);
+    int i = 0;
+    while (i < 5) {
+      iter.next();
+      i++;
+    }
+    List<Node> l1 = new ArrayList<Node>() {
+      {
+        add(nodeone_fr);
+      }
+    };
 
-		Mockito.doReturn(iter).when(wcmComposerImplSpy).getViewableContents(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(HashMap.class), Mockito.any(SessionProvider.class), Mockito.any(Boolean.class));
+    List<Node> l2 = new ArrayList<Node>() {
+      {
+        add(nodeone_en);
+      }
+    };
 
-		Mockito.doReturn(l1).when(wcmComposerImplSpy).getRealTranslationNodes(nodeone_en);
-		Mockito.doReturn(l2).when(wcmComposerImplSpy).getRealTranslationNodes(nodeone_fr);
-		Result result = wcmComposerImplSpy.getPaginatedContents(nodeLocation, filters, sessionProvider);
-		assertEquals(1, result.getNodes().size());
-	}
+    Mockito.doReturn(iter)
+           .when(wcmComposerImplSpy)
+           .getViewableContents(Mockito.any(String.class),
+                                Mockito.any(String.class),
+                                Mockito.any(HashMap.class),
+                                Mockito.any(SessionProvider.class),
+                                Mockito.any(Boolean.class));
 
-	public void testgetContentsShouldRemoveDuplicatedNodes() throws Exception {
-		HashMap<String, String> context = new HashMap<String, String>();
-		context.put("visibility", "true");
+    Mockito.doReturn(l1).when(wcmComposerImplSpy).getRealTranslationNodes(nodeone_en);
+    Mockito.doReturn(l2).when(wcmComposerImplSpy).getRealTranslationNodes(nodeone_fr);
+    Result result = wcmComposerImplSpy.getPaginatedContents(nodeLocation, filters, sessionProvider);
+    assertEquals(1, result.getNodes().size());
+  }
 
-		HashMap<String, String> filters = new HashMap<>();
-		String folderPath = "repository:collaboration:/sites content/live";
-		NodeLocation  nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
-		String workspace = nodeLocation.getWorkspace();
-		Node rootNode = session.getRootNode();//getNode("/sites content/live");
+  public void testgetContentsShouldRemoveDuplicatedNodes() throws Exception {
+    HashMap<String, String> context = new HashMap<String, String>();
+    context.put("visibility", "true");
 
-		Node nodeone_en = rootNode.addNode("nodeone_en", NodetypeConstant.EXO_WEBCONTENT);
-		nodeone_en.setProperty(NodetypeConstant.EXO_LANGUAGE, "en");
-		nodeone_en.setProperty(NodetypeConstant.EXO_TITLE,"node one en");
-		publicationService.enrollNodeInLifecycle(nodeone_en, plugin_.getLifecycleName());
-		publicationService.changeState(nodeone_en, PUBLISHED, context);
+    HashMap<String, String> filters = new HashMap<>();
+    String folderPath = "repository:collaboration:/sites content/live";
+    NodeLocation nodeLocation = NodeLocation.getNodeLocationByExpression(folderPath);
+    String workspace = nodeLocation.getWorkspace();
+    Node rootNode = session.getRootNode();// getNode("/sites content/live");
 
-		Node nodeone_fr = rootNode.addNode("nodeone_fr", NodetypeConstant.EXO_WEBCONTENT);
-		nodeone_fr.setProperty(NodetypeConstant.EXO_LANGUAGE, "fr");
-		nodeone_fr.setProperty(NodetypeConstant.EXO_TITLE,"node one fr");
-		publicationService.enrollNodeInLifecycle(nodeone_fr, plugin_.getLifecycleName());
-		publicationService.changeState(nodeone_fr, PUBLISHED, context);
-		NodeIterator iter = rootNode.getNodes();
-		WCMComposerImpl wcmComposerImplSpy = Mockito.spy(wcmComposerService);
-		int i =0;
-		while (i < 5){
-			iter.next();
-			i++;
-		}
-		List<Node> l1 = new ArrayList<Node>(){{add(nodeone_fr);}};
+    Node nodeone_en = rootNode.addNode("nodeone_en", NodetypeConstant.EXO_WEBCONTENT);
+    nodeone_en.setProperty(NodetypeConstant.EXO_LANGUAGE, "en");
+    nodeone_en.setProperty(NodetypeConstant.EXO_TITLE, "node one en");
+    publicationService.enrollNodeInLifecycle(nodeone_en, plugin_.getLifecycleName());
+    publicationService.changeState(nodeone_en, PUBLISHED, context);
 
-		List<Node> l2 = new ArrayList<Node>(){{add(nodeone_en);}};
+    Node nodeone_fr = rootNode.addNode("nodeone_fr", NodetypeConstant.EXO_WEBCONTENT);
+    nodeone_fr.setProperty(NodetypeConstant.EXO_LANGUAGE, "fr");
+    nodeone_fr.setProperty(NodetypeConstant.EXO_TITLE, "node one fr");
+    publicationService.enrollNodeInLifecycle(nodeone_fr, plugin_.getLifecycleName());
+    publicationService.changeState(nodeone_fr, PUBLISHED, context);
+    NodeIterator iter = rootNode.getNodes();
+    WCMComposerImpl wcmComposerImplSpy = Mockito.spy(wcmComposerService);
+    int i = 0;
+    while (i < 5) {
+      iter.next();
+      i++;
+    }
+    List<Node> l1 = new ArrayList<Node>() {
+      {
+        add(nodeone_fr);
+      }
+    };
 
-		Mockito.doReturn(iter).when(wcmComposerImplSpy).getViewableContents(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(HashMap.class), Mockito.any(SessionProvider.class), Mockito.any(Boolean.class));
-		Mockito.doReturn(l1).when(wcmComposerImplSpy).getRealTranslationNodes(nodeone_en);
-		Mockito.doReturn(l2).when(wcmComposerImplSpy).getRealTranslationNodes(nodeone_fr);		List<Node> nodes = wcmComposerImplSpy.getContents(workspace,folderPath, filters, sessionProvider);
-		assertEquals(1, nodes.size());
-	}
-	public void tearDown() throws Exception {
-		super.tearDown();
-	}
+    List<Node> l2 = new ArrayList<Node>() {
+      {
+        add(nodeone_en);
+      }
+    };
+
+    Mockito.doReturn(iter)
+           .when(wcmComposerImplSpy)
+           .getViewableContents(Mockito.any(String.class),
+                                Mockito.any(String.class),
+                                Mockito.any(HashMap.class),
+                                Mockito.any(SessionProvider.class),
+                                Mockito.any(Boolean.class));
+    Mockito.doReturn(l1).when(wcmComposerImplSpy).getRealTranslationNodes(nodeone_en);
+    Mockito.doReturn(l2).when(wcmComposerImplSpy).getRealTranslationNodes(nodeone_fr);
+    List<Node> nodes = wcmComposerImplSpy.getContents(workspace, folderPath, filters, sessionProvider);
+    assertEquals(1, nodes.size());
+  }
+
+  public void tearDown() throws Exception {
+    super.tearDown();
+  }
 
 }
