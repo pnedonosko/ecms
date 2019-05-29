597b7479c3979f194efc74c326e739a813f9bda3
wael ghannem <wghannem@exoplatform.com>
2019-05-29 10:23:33 +0100

    fix/ECMS-7628 : clean code.

--- core/publication/src/main/java/org/exoplatform/services/wcm/publication/WCMComposer.java
+++ core/publication/src/main/java/org/exoplatform/services/wcm/publication/WCMComposer.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import javax.jcr.Node;
-import javax.jcr.NodeIterator;
 
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.wcm.core.NodeLocation;
